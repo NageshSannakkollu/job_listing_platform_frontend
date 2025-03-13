4d64a4f2efec91ae8fe1bd7ea66219c815964ab8
NageshSannakkollu <122163329+NageshSannakkollu@users.noreply.github.com>
2025-03-13 10:04:53 +0530

    job listing frontend commit

--- src/components/LandingPage/index.js
+++ src/components/LandingPage/index.js
@@ -149,7 +149,7 @@ const LandingPage =  () => {
     useEffect(()=>{
         const getProfileDetails = async() => {
             try {
-                const response = await fetch("http://localhost:3025/api/auth/profile",{
+                const response = await fetch("https://67d25c961161f3c2ca952340--job-listing-backend.netlify.app/api/auth/profile",{
                     method:'GET',
                     headers:{
                         "Authorization":`Bearer ${jwtToken}`,
@@ -168,7 +168,7 @@ const LandingPage =  () => {
         const getJobsList = async() => {
             if(deleteJobFormList){
                 setDeleteJob(false)
-            const response = await axios.get('http://localhost:3025/api/jobs')
+            const response = await axios.get('https://67d25c961161f3c2ca952340--job-listing-backend.netlify.app/api/jobs')
             setSampleJobsListInfo(response.data)
         }
     }
@@ -179,7 +179,7 @@ const LandingPage =  () => {
 
     const deleteJob = (id) => {
     // console.log("delete Id:",id)
-    axios.delete(`http://localhost:3025/api/jobs/${id}`)
+    axios.delete(`https://67d25c961161f3c2ca952340--job-listing-backend.netlify.app/api/jobs/${id}`)
     .then(response => {
       console.log(response)
       setDeleteJob(true)
@@ -210,7 +210,6 @@ const LandingPage =  () => {
             console.log("filterJobs:",filterJobs)
         }
     }
-
     const clickOnClear = () => {
         
     }
@@ -245,8 +244,8 @@ const LandingPage =  () => {
                     <button type='button' className='clear_filter_button' onClick={clickOnClear}>Clear</button>
                 </>
             }
-            </div>
-            </div>
+                </div>
+              </div>
             </div>
         </div>
         <ul className='job_info_card_container'>
